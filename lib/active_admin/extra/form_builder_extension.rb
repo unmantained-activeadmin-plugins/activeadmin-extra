6a20b46d9ff2f7838e91a5fd37cc9d4172a62287
Stefano Verna <stefano.verna@welaika.com>
2012-12-13 14:24:12 +0100

    Added f.destroy formtastic helper

--- lib/active_admin/extra/form_builder_extension.rb
+++ lib/active_admin/extra/form_builder_extension.rb
@@ -3,6 +3,13 @@ module ActiveAdmin
     module FormBuilderExtension
       extend ActiveSupport::Concern
 
+      def destroy(options = {})
+        unless object.new_record?
+          input :_destroy, { :as => :boolean }.reverse_merge(options)
+        end
+        form_buffers.last
+      end
+
       module ClassMethods
       end
     end
